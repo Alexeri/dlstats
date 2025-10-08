e8e9aa10b94b1e49da177e405f2436cfb27df344
Alexander <32795918+Alexeri@users.noreply.github.com>
2025-10-08 12:30:45 +0200

    added tanstack query, basic fetch and render for hero winrates/hero assets

--- src/app/tierlist/page.tsx
+++ src/app/tierlist/page.tsx
@@ -1,7 +1,19 @@
-export default function TierlistPage() {
-    return (
-        <div>
-            Enter
-        </div>
-    );
+import { getQueryClient } from "@/app/get-query-client";
+import Tierlist from "@/components/tierlist/tierlist";
+import { getTierListData } from "@/lib/data/heroes";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+export default async function TierlistPage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["tierlist"],
+    queryFn: () => getTierListData(queryClient),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Tierlist />
+    </HydrationBoundary>
+  );
 }
